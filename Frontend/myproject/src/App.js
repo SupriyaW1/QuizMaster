c9f2c172dd915e164ccc9b4506974fffba5fa369
Saurabh Patil <saurabh9370patil@gmail.com>
2024-02-09 22:27:51 +0530

    use case added

--- Frontend/myproject/src/App.js
+++ Frontend/myproject/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Link, Routes } from 'react-router-dom';
+import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ul className="nav navbar"> 
+           <li className="nav-item">
+            <Link to={"/signup"} className="nav-link">Sign Up</Link> 
+           </li>
+  
+       </ul>
+       <Routes>
+       <Route path="/signup" element={<Sign/>} />
+       </Routes>
+              <h1 className="bg-primary text-white">Home Page </h1>
+
     </div>
   );
 }
